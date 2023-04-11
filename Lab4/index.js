0dfe4c3df91feaa011df43fe7582c9725b3a088b
Marcin Leszczyński <leszczynski-marcin@wp.pl>
2023-04-11 21:01:41 +0200

    lab4

---
+++ Lab4/index.js
@@ -0,0 +1,99 @@
+const session = require("express-session");
+const express = require("express");
+const http = require("http");
+const uuid = require("uuid");
+
+const { WebSocketServer } = require("ws");
+
+function onSocketError(err) {
+  console.error(err);
+}
+
+const app = express();
+const map = new Map();
+const rooms = new Map();
+//
+// We need the same instance of the session parser in express and
+// WebSocket server.
+//
+const sessionParser = session({
+  saveUninitialized: false,
+  secret: "secret",
+  resave: false,
+});
+
+//
+// Serve static files from the 'public' folder.
+//
+app.use(express.static("public"));
+app.use(sessionParser);
+
+app.get("/room", function (request, response) {
+  const roomId = request.params.roomId;
+});
+
+//
+// Create an HTTP server.
+//
+const server = http.createServer(app);
+
+//
+// Create a WebSocket server completely detached from the HTTP server.
+//
+const wss = new WebSocketServer({ port: 8081 });
+
+server.on("upgrade", function (request, socket, head) {
+  socket.on("error", onSocketError);
+
+  console.log("Parsing session from request...");
+
+  sessionParser(request, {}, () => {
+    console.log("Session is parsed!");
+
+    socket.removeListener("error", onSocketError);
+
+    wss.handleUpgrade(request, socket, head, function (ws) {
+      wss.emit("connection", ws, request);
+    });
+  });
+});
+
+function sendToSession(id, msg) {
+  wss.clients.forEach(function each(client) {
+    if (client === map.get(id) && client.readyState === 1) {
+      client.send(msg);
+    }
+  });
+}
+
+function sendToAllConnectedSessions(msg) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === 1) {
+      client.send(msg);
+    }
+  });
+}
+
+wss.on("connection", function (ws, request) {
+  const id = uuid.v4();
+  console.log(`Updating session for user ${id}`);
+  map.set(id, ws);
+  ws.on("error", console.error);
+
+  ws.on("message", function (message) {
+    sendToAllConnectedSessions(message);
+    console.log(`Received message ${message}`);
+  });
+
+  ws.on("close", function () {
+    map.delete(id);
+    console.log(`Client disconnected`);
+  });
+});
+
+//
+// Start the server.
+//
+server.listen(8080, function () {
+  console.log("Listening on http://localhost:8080");
+});
---
+++ Lab4/public/app.js
@@ -0,0 +1,50 @@
+(function () {
+  const messages = document.querySelector("#messages");
+  const wsButton = document.querySelector("#wsButton");
+  const wsSendButton = document.querySelector("#wsSendButton");
+  const logout = document.querySelector("#logout");
+
+  function showMessage(message) {
+    messages.textContent += `\n${message}`;
+    messages.scrollTop = messages.scrollHeight;
+  }
+
+  let ws;
+
+  wsButton.onclick = function () {
+    if (ws) {
+      ws.onerror = ws.onopen = ws.onclose = null;
+      ws.close();
+    }
+
+    ws = new WebSocket(`ws:/${location.host}`);
+    ws.onerror = function () {
+      showMessage("WebSocket error");
+    };
+    ws.onopen = function () {
+      showMessage("WebSocket connection established");
+    };
+    ws.onmessage = async function (data) {
+      const msg = await data.data.text();
+      showMessage(`Recived ${msg}`);
+    };
+    ws.onclose = function () {
+      showMessage("WebSocket connection closed");
+      ws = null;
+    };
+  };
+
+  wsSendButton.onclick = function () {
+    if (!ws) {
+      showMessage("No WebSocket connection");
+      return;
+    }
+    const message = document.getElementById("messageinput").value;
+    ws.send(message);
+    showMessage(`Sent ${message}`);
+  };
+
+  logout.onclick = function () {
+    ws.close();
+  };
+})();
