d23f5aeab2be975f5d48bc537b294d15a75d6a67
Marcin Leszczyński <leszczynski-marcin@wp.pl>
2023-04-24 17:01:43 +0200

    fix getGoogleUser

--- utils/getGoogleUser.js
+++ utils/getGoogleUser.js
@@ -1,5 +1,5 @@
 async function getGoogleUser(id_token, access_token) {
-  const { data } = await fetch(
+  const response = await fetch(
     `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
     {
       method: "GET",
