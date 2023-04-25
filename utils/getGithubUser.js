b0513bab37b254c8d09b26de7c2a7bc7f2e08fb4
Marcin Leszczyński <leszczynski-marcin@wp.pl>
2023-04-25 17:59:37 +0200

    add api for github email

--- utils/getGithubUser.js
+++ utils/getGithubUser.js
@@ -6,24 +6,25 @@ const getGithubUser = async (access_token) => {
     },
   });
   const result = await response.json();
-  console.log(result);
   if (response.status !== 200) {
     throw new Error(result.message);
   }
-  // if (has_user_email_scope) {
-  //   const emailResponse = await fetch(`https://api.github.com/user/emails`, {
-  //     method: "GET",
-  //     headers: {
-  //       Authorization: `Bearer ${access_token}`,
-  //     },
-  //   });
-  //   const emailResult = await emailResponse.json();
-  //   if (emailResponse.status !== 200) {
-  //     console.log(result.error);
-  //     throw new Error(result.error.message);
-  //   }
 
-  // }
+  if (result.email === null) {
+    const responseEmail = await fetch(`https://api.github.com/user/emails`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    const resultEmail = await responseEmail.json();
+    if (responseEmail.status !== 200) {
+      throw new Error(result.message);
+    }
+    result.email = resultEmail[0].email;
+  }
+  console.log(result);
+
   return result;
 };
 
