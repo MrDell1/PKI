7faed07c09c467eb52644ce1fc7d3f96b4ce6715
Marcin Leszczyński <leszczynski-marcin@wp.pl>
2023-04-25 12:33:19 +0200

    change Navigate to useNavigate

--- Lab6/src/routes/Google/GoogleAuth/GoogleAuth.tsx
+++ Lab6/src/routes/Google/GoogleAuth/GoogleAuth.tsx
@@ -3,15 +3,18 @@ import { useAnonService } from "@services/SessionService";
 import { useMutation } from "@tanstack/react-query";
 import { paths } from "@utils/paths";
 import { ReactElement } from "react";
-import { Navigate } from "react-router";
+import { useNavigate } from "react-router";
 
 export const GoogleAuth = (): ReactElement => {
   const anonService = useAnonService();
   const { mutate } = useMutation(anonService.oauthGoogle);
-
+  const navigate = useNavigate();
   mutate(window.location.search, {
     onError: () => {
-      return <Navigate replace to={paths.signIn} />;
+      navigate(paths.signIn);
+    },
+    onSuccess: () => {
+      navigate(paths.resources);
     },
   });
 
